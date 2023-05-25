c5e0f487a597d1ff49f7339e4113c7d6a8b2eacf
dvnggak <devanggakertawijaya@gmail.com>
2023-05-25 21:53:17 +0700

    New Feature - Website : request handler for registring an Admin & Hashing the plain Password before go to db

--- controller/admin/landing_page_controller.go
+++ controller/admin/landing_page_controller.go
@@ -1,13 +1,43 @@
 package admin
 
 import (
-	"net/http"
-
+	"github.com/agriplant/config"
+	"github.com/agriplant/model"
 	"github.com/labstack/echo/v4"
 )
 
-func Admin_Hello_World(c echo.Context) error {
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"message": "Hello World, welcome to the admin endpoint",
+func CreateAdmin(c echo.Context) error {
+	admin := model.Admin{}
+
+	c.Bind(&admin)
+
+	admin.BeforeCreateAdmin(config.DB)
+
+	if err := config.DB.Save(&admin).Error; err != nil {
+		return c.JSON(500, map[string]interface{}{
+			"message": "Failed to create admin",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.JSON(200, map[string]interface{}{
+		"message": "Success create admin",
+		"data":    admin,
+	})
+}
+
+func GetAdmins(c echo.Context) error {
+	admins := []model.Admin{}
+
+	if err := config.DB.Find(&admins).Error; err != nil {
+		return c.JSON(500, map[string]interface{}{
+			"message": "Failed to get admins",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.JSON(200, map[string]interface{}{
+		"message": "Success get admins",
+		"data":    admins,
 	})
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/agriplant
 
 go 1.20
 
-require gorm.io/driver/mysql v1.5.1
+require (
+	golang.org/x/crypto v0.6.0
+	gorm.io/driver/mysql v1.5.1
+)
 
 require (
 	github.com/labstack/gommon v0.4.0 // indirect
@@ -10,7 +13,6 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
@@ -21,5 +23,5 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/echo/v4 v4.10.2
-	gorm.io/gorm v1.25.1 // indirect
+	gorm.io/gorm v1.25.1
 )
