15369c3e63a6557b26345f1388949ca683c4fb9f
widhofaisal <widhofaisal26@gmail.com>
2023-05-30 13:55:54 +0700

    deploy on gcp and testing no 7

--- config/config.go
+++ config/config.go
@@ -30,7 +30,7 @@ func InitDB() {
 		DB_Username: "developergolang",
 		DB_Password: "plantagridb123",
 		DB_Port:     "3306",
-		DB_Host:     "database-agriplant.ctbpmx2n7zth.ap-southeast-2.rds.amazonaws.com",
+		DB_Host:     "mysql",
 		DB_Name:     "agriplant_db",
 	}
 
