2ea0c4e5be34b3167f1e99a5a151670e9b76e96e
dvnggak <devanggakertawijaya@gmail.com>
2023-06-03 15:23:30 +0700

    web/fix/products/GetProductByID - fix log error and custom response

--- controller/admin/product_page_controller.go
+++ controller/admin/product_page_controller.go
@@ -149,20 +149,58 @@ func GetProductByID(c echo.Context) error {
 	// If product not found, return error
 	if err := config.DB.First(&product, id).Error; err != nil {
 		log.Print(color.RedString(err.Error()))
-		return echo.NewHTTPError(http.StatusInternalServerError)
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"status":  500,
+			"message": "internal server error",
+		})
 	}
 
 	// Populate Pictures field for each product
 	config.DB.Model(&product).Association("Pictures").Find(&product.Pictures)
 
-	// remove article_id from product_pictures
-	for i := 0; i < len(product.Pictures); i++ {
-		product.Pictures[i].ArticleID = nil
+	// Extract picture URLs
+	pictureURLs := make([]string, len(product.Pictures))
+	for i, pic := range product.Pictures {
+		pictureURLs[i] = pic.URL
+	}
+
+	response := struct {
+		ID          uint     `json:"id"`
+		Pictures    []string `json:"product_pictures"`
+		Name        string   `json:"product_name"`
+		Category    string   `json:"product_category"`
+		Description string   `json:"product_description"`
+		Price       int      `json:"product_price"`
+		Status      bool     `json:"product_status"`
+		Brand       string   `json:"product_brand"`
+		Condition   string   `json:"product_condition"`
+		Unit        int      `json:"product_unit"`
+		Weight      int      `json:"product_weight"`
+		Form        string   `json:"product_form"`
+		SellerName  string   `json:"product_seller_name"`
+		SellerPhone string   `json:"product_seller_phone"`
+		AdminID     uint     `json:"admin_id"`
+	}{
+		ID:          product.ID,
+		Pictures:    pictureURLs,
+		Name:        product.Name,
+		Category:    product.Category,
+		Description: product.Description,
+		Price:       product.Price,
+		Status:      product.Status,
+		Brand:       product.Brand,
+		Condition:   product.Condition,
+		Unit:        product.Unit,
+		Weight:      product.Weight,
+		Form:        product.Form,
+		SellerName:  product.SellerName,
+		SellerPhone: product.SellerPhone,
+		AdminID:     product.AdminID,
 	}
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"message": "success",
-		"data":    product,
+		"data":    response,
 	})
 }
 
