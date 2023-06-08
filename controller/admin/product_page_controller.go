a312fe01d6c902ede5e1badee0e06d58a4056fdd
dvnggak <devanggakertawijaya@gmail.com>
2023-06-08 23:13:15 +0700

    CreateProduct - Fix : remove admin_id from response body

--- controller/admin/product_page_controller.go
+++ controller/admin/product_page_controller.go
@@ -136,7 +136,6 @@ func CreateProduct(c echo.Context) error {
 		Form        string   `json:"product_form"`
 		SellerName  string   `json:"product_seller_name"`
 		SellerPhone string   `json:"product_seller_phone"`
-		AdminID     uint     `json:"admin_id"`
 	}{
 		ID:          product.ID,
 		Pictures:    pictureURLs,
@@ -152,7 +151,6 @@ func CreateProduct(c echo.Context) error {
 		Form:        product.Form,
 		SellerName:  product.SellerName,
 		SellerPhone: product.SellerPhone,
-		AdminID:     product.AdminID,
 	}
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
