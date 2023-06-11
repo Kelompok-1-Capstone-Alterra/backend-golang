ca3cf6d9d6c8a974f720ecb60ca0089d56d8f733
widhofaisal <widhofaisal26@gmail.com>
2023-06-11 22:39:26 +0700

    change config to gcp

--- config/config.go
+++ config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/agriplant/model"
 	"github.com/joho/godotenv"
@@ -30,11 +29,11 @@ type Config struct {
 
 func InitDB() {
 	config := Config{
-		DB_Username: os.Getenv("DB_USERNAME"),
-		DB_Password: os.Getenv("DB_PASSWORD"),
-		DB_Port:     os.Getenv("DB_PORT"),
-		DB_Host:     os.Getenv("DB_HOST"),
-		DB_Name:     os.Getenv("DB_DB"),
+		DB_Username: "developergolang",
+		DB_Password: "plantagridb123",
+		DB_Port:     "3306",
+		DB_Host:     "mysql",
+		DB_Name:     "agriplant_db",
 	}
 
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
@@ -60,7 +59,7 @@ func InitialMigration() {
 		&model.Product{},
 		&model.Weather{},
 		&model.InfoWeather{},
-    &model.LikedArticles{},
+		&model.LikedArticles{},
 		&model.Plant{},
 		&model.WateringInfo{},
 		&model.TemperatureInfo{},
@@ -73,7 +72,7 @@ func InitialMigration() {
 		&model.MyPlant{},
 		&model.Watering{},
 		&model.Fertilizing{},
-    &model.Complaints{},
+		&model.Complaints{},
 		&model.Suggestions{},
 	)
 }
